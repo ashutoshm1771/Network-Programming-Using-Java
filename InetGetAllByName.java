bec41c88f9d7f2d4c481f6150e6bced9625c6209
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2019-12-16 11:44:11 +0530

    Add files via upload

---
+++ InetGetAllByName.java
@@ -0,0 +1,19 @@
+import java.net.*;
+public class InetGetAllByName
+{
+public static void main(String args[])
+{
+try{
+InetAddress ia[]=InetAddress.getAllByName(args[0]);
+for(int i=0;i<ia.length;i++)
+{
+System.out.println(ia[i]);
+System.out.println(ia[i].getHostName());
+System.out.println(ia[i].getHostAddress());
+}
+}
+catch(Exception e){
+System.out.println(e);
+}
+}
+}
