d632183993b0856bfac2d18b4a41bc54699d0cbf
Ashutosh Mishra <48184601+ashutoshm1771@users.noreply.github.com>
2020-01-07 14:00:27 +0530

    Add files via upload

---
+++ JavaUrlConnectionReader.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.io.*;
+
+
+public class JavaUrlConnectionReader
+{
+  public static void main(String[] args)
+  {
+    String output  = getUrlContents("http://localhost:8080/");
+    System.out.println(output);
+  }
+
+  private static String getUrlContents(String theUrl)
+  {
+    StringBuilder content = new StringBuilder();
+
+    // many of these calls can throw exceptions, so i've just
+    // wrapped them all in one try/catch statement.
+    try
+    {
+      // create a url object
+      URL url = new URL(theUrl);
+
+      // create a urlconnection object
+      URLConnection urlConnection = url.openConnection();
+
+      // wrap the urlconnection in a bufferedreader
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+      String line;
+
+      // read from the urlconnection via the bufferedreader
+      while ((line = bufferedReader.readLine()) != null)
+      {
+        content.append(line + "\n");
+      }
+      bufferedReader.close();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    return content.toString();
+  }
+}
